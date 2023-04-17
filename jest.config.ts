371d763a646c9bbba7ca8aacd8730fbd94625738
MBuchalik <dev@martin-buchalik.de>
2023-04-17 09:58:03 +0200

    Do not execute tests from the 'dist/' directory

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,7 @@ import { Config } from '@jest/types';
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  modulePathIgnorePatterns: ['<rootDir>/dist/']
 };
 
 module.exports = config;
